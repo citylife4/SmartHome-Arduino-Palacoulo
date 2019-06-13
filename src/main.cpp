e6bc3b7e14a38a992bc6098155b957cea6f0e041
Jose Valverde <delfimvalverde@gmail.com>
2019-06-13 20:16:03 +0100

    Changes on the script - mostly syntax and organization of the script

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,6 @@
 #include <SoftwareSerial.h>
 #include <EEPROM.h>
 
-//RS485 Defines
-
 // the data we broadcast to each other device
 struct
 {
@@ -11,22 +9,29 @@ struct
   byte switches [10];
 }  message;
 
+//Serial Connection Defines
+const unsigned long BAUD_RATE = 9600;
+
 const unsigned long RS485_BAUD_RATE = 9600;
+
+unsigned long lastMessageTime;
+unsigned long lastCommsTime;
+unsigned long randomTime;
+
+// action pins (demo)
+const int REED_PIN = 2; // Pin connected to reed switch
+const int LED_PIN = 13; // LED pin - active-high
+
 const float TIME_PER_BYTE = 1.0 / (RS485_BAUD_RATE / 10.0);  // seconds per sending one byt
 // software serial pins
 const byte RO_PIN = 8;
 const byte DI_PIN = 9;
 // transmit enable
 const byte XMIT_ENABLE_PIN = 10;
+const byte ENABLE_PIN = 4;
 
-
-byte nextAddress;
-unsigned long lastMessageTime;
-unsigned long lastCommsTime;
-unsigned long randomTime;
 byte inByte = 0;         // incoming serial byte
-
-SoftwareSerial rs485 ( RO_PIN, DI_PIN);  // receive pin, transmit pin
+byte nextAddress;
 
 // from EEPROM
 byte myAddress;        // who we are
@@ -46,16 +51,61 @@ int fRead () {
 }
 
 // RS485 library instance
-RS485 rs485Channel (fRead, fAvailable, fWrite, 20);
+RS485          rs485Channel (fRead, fAvailable, fWrite, 20);
+SoftwareSerial rs485        ( RO_PIN, DI_PIN);  // receive pin, transmit pin
 
-// action pins (demo)
-const int REED_PIN = 2; // Pin connected to reed switch
-const int LED_PIN = 13; // LED pin - active-high
-const byte ENABLE_PIN = 4;
-String inString = "";
 
-//Serial Connection Defines
-const unsigned long BAUD_RATE = 9600;
+// Here to process an incoming message
+void processSerialData() {
+  // handle the incoming message, depending on who it is from and the data in it
+  // we cannot receive a message from ourself
+  // someone must have given two devices the same address
+  if (message.address == myAddress)
+  {
+    Serial.println ("Problems");
+  }
+  // make our LED match the switch of the previous device in sequence
+
+/*
+  Serial.println (message.switches [0]);
+  Serial.println (message.switches [1]);
+  int myMessage = ((message.switches [1]<<4)+message.switches [0]);
+*/
+  int myMessage = message.switches [0];
+  Serial.println (myMessage);
+
+} // end of processMessage
+
+//Where we process the data received on the RS485 Channel
+void UpdateRS485Info() {
+    memset (&message, 0, sizeof message);
+    int len = rs485Channel.getLength ();
+    if (len > sizeof message)
+      len = sizeof message;
+    memcpy (&message, rs485Channel.getData (), len);
+    lastMessageTime = micros ();
+    processSerialData ();
+}
+
+// Here to send our own message
+void sendRS485Message ()
+{
+  memset (&message, 0, sizeof message);
+  message.address = myAddress;
+
+  int i=1;
+  //Check Serial connection for data
+  while (Serial.available() > 0) {
+    int serialIn = Serial.read();
+    message.switches[i] = serialIn;
+    i++;
+  }
+
+  digitalWrite (XMIT_ENABLE_PIN, HIGH);  // enable sending
+  rs485Channel.sendMsg ((byte *) &message, sizeof message);
+  digitalWrite (XMIT_ENABLE_PIN, LOW);  // disable sending
+
+}  // end of sendMessage
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -92,45 +142,6 @@ void setup() {
   digitalWrite (LED_PIN, HIGH);
 }
 
-// Here to process an incoming message
-void processSerialData() {
-  // handle the incoming message, depending on who it is from and the data in it
-  // we cannot receive a message from ourself
-  // someone must have given two devices the same address
-  if (message.address == myAddress)
-  {
-    Serial.println ("Problems");
-    //digitalWrite (ERROR_PIN, HIGH);
-    //while (true)
-    //  { }  // give up
-  }
-  // make our LED match the switch of the previous device in sequence
-
-  Serial.println (int(message.switches [0]));
-  Serial.println (message.address);
-  digitalWrite (LED_PIN, message.switches [0]);
-} // end of processMessage
-
-// Here to send our own message
-void sendRS485Message ()
-{
-  memset (&message, 0, sizeof message);
-  message.address = myAddress;
-  int inChar = Serial.read();
-  if (isDigit(inChar)) {
-    // convert the incoming byte to a char and add it to the string:
-    inString += (char)inChar;
-  }
-  message.switches[0] = (char) inString.toInt();
-  Serial.write(inString.toInt());
-  inString = "";
-
-  digitalWrite (XMIT_ENABLE_PIN, HIGH);  // enable sending
-  rs485Channel.sendMsg ((byte *) &message, sizeof message);
-  digitalWrite (XMIT_ENABLE_PIN, LOW);  // disable sending
-
-}  // end of sendMessage
-
 // the loop function runs over and over again forever
 void loop() {
   //Check Serial connection for data
@@ -139,15 +150,7 @@ void loop() {
   } //end serial.available
   //Check RS connection for data
   if (rs485Channel.update ()) {
-    memset (&message, 0, sizeof message);
-    int len = rs485Channel.getLength ();
-    if (len > sizeof message)
-      len = sizeof message;
-    Serial.write("bla");
-    Serial.write(message.switches[0]);
-    memcpy (&message, rs485Channel.getData (), len);
-    lastMessageTime = micros ();
-    processSerialData ();
+    UpdateRS485Info();
   }  // end of message completely received
   //Check outputs from GPIOs
 
